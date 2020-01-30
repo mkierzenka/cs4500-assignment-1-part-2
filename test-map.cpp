2e55226c525b2b9a0a4ba79e9196b3caafe59977
kaylindevchand <kaydevchand@gmail.com>
2020-01-30 12:33:11 -0500

    Remove replace tests. Add tests for map obj to obj

--- test-map.cpp
+++ test-map.cpp
@@ -3,6 +3,7 @@
 #include "object.h"  // Your file with the CwC declaration of Object
 #include "string.h"  // Your file with the String class
 #include "map.h"
+#include "array.h"
 #include <assert.h>
  
 // This test class was added to allow for easier testing
@@ -26,30 +27,43 @@ public:
   void t_true(bool p) { if (!p) FAIL(); }
   void t_false(bool p) { if (p) FAIL(); }
 
-  void test_replace_1() {
-    String * u = new String("HelloWorld");
-    MapStrStr * mss = new MapStrStr();
-    mss->put(s, t);
-    t_true(t->equals(mss->get(s)));
-    mss->put(s, u);
-    t_true(u->equals(mss->get(s)));
-    t_true(mss->size() == 1);
-    delete u;
-    delete mss;
-    OK("test_replace_1");
-  }
+    void test_put_0() {
+      Object * a = new Object();
+      Object * b = new Object();
+      Map * mss = new Map();
+      mss->put(s, t);
+      mss->put(a, b);
+      t_true(mss->size() == 2);
+      t_true(mss->contains_key(s));
+      t_true(mss->contains_key(a));
+      t_true(mss->get(a)->equals(b));
 
-  void test_replace_2() {
-    Object * u = new String("hi");
-    MapStrObj * mss = new MapStrObj();
+      Object * c = new Object();
+      Object * d = mss->put(a, c);
+      t_true(mss->get(a)->equals(c));
+      t_true(d->equals(b));
+
+      OK("test_put_0");
+
+    }
+
+    void test_remove_0() {
+    Object * a = new Object();
+    Object * b = new Object();
+    Map * mss = new Map();
     mss->put(s, t);
-    t_true(t->equals(mss->get(s)));
-    mss->put(s, u);
-    t_true(u->equals(mss->get(s)));
+    mss->put(a, b);
+    t_true(mss->size() == 2);
+    t_true(mss->contains_key(s));
+    t_true(mss->contains_key(a));
+    Object * q = mss->remove(s);
+    t_true(t->equals(q));
+    t_false(mss->contains_key(s));
     t_true(mss->size() == 1);
-    delete u;
+    delete a;
+    delete b;
     delete mss;
-    OK("test_replace_2");
+    OK("test_remove_0");
   }
 
   void test_remove_1() {
@@ -90,22 +104,42 @@ public:
     OK("test_remove_2");
   }
 
+  void test_get_0() {
+    Object * a = new Object();
+    Object * b = new Object();
+    Map * mss = new Map();
+    mss->put(a, b);
+
+    Array* keys = mss->get_keys();
+    Array* values = mss->get_values();
+    t_true(b->equals(values->get(0)));
+    t_true(a->equals(keys->get(0)));
+
+    delete a;
+    delete b;
+    delete mss;
+    delete keys;
+    delete values;
+
+    OK("test_get_0");
+  }
+
   void test_get_1() {
     String * u = new String("HelloWorld");
     String * w = new String("w");
     MapStrStr * mss = new MapStrStr();
     mss->put(s, t);
 
-    String** keys = mss->get_keys();
-    String** values = mss->get_values();
-    t_true(t->equals(values[0]));
-    t_true(s->equals(keys[0]));
+    Array* keys = mss->get_keys();
+    Array* values = mss->get_values();
+    t_true(t->equals(values->get(0)));
+    t_true(s->equals(keys->get(0)));
 
     delete u;
     delete w;
     delete mss;
-    delete[] keys;
-    delete[] values;
+    delete keys;
+    delete values;
 
     OK("test_get_1");
   }
@@ -116,10 +150,10 @@ public:
     MapStrObj * mss = new MapStrObj();
     mss->put(s, t);
 
-    String** keys = mss->get_keys();
-    Object** values = mss->get_values();
-    t_true(t->equals(values[0]));
-    t_true(s->equals(keys[0]));
+    Array* keys = mss->get_keys();
+    Array* values = mss->get_values();
+    t_true(t->equals(values->get(0)));
+    t_true(s->equals(keys->get(0)));
 
     delete u;
     delete w;
@@ -132,10 +166,10 @@ public:
 
 int main(int argc, char** argv) {
   Test* test = new Test();
-  test->test_replace_1();
-  test->test_replace_2();
+  test->test_remove_0();
   test->test_remove_1();
   test->test_remove_2();
+  test->test_get_0();
   test->test_get_1();
   test->test_get_2();
   delete test;
